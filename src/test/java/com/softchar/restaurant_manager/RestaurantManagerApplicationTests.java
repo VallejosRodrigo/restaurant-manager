52b40b619e737fc1d6406a7e0739b8af7cf7f5ac
Tu Rodrigo <vallejos.rodrigo@outlook.com>
2024-06-21 21:02:14 -0300

    first commit

---
+++ src/test/java/com/softchar/restaurant_manager/RestaurantManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softchar.restaurant_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
